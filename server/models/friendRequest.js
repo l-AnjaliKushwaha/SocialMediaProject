b461de8eccd7982dfc58a220fbdf49530724204d
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-04-21 12:18:47 +0530

    commentModelSchema

---
+++ server/models/friendRequest.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from "mongoose";
+
+const requestSchema = Schema({
+  requestTo: { type: Schema.Types.ObjectId, ref: "Users" },
+  requestFrom: { type: Schema.Types.ObjectId, ref: "Users" },
+  requestStatus: { type: String, default: "pending" },
+},
+
+{ timestamps: true }
+
+);
+
+
+const FriendRequest = mongoose.model("FriendRequest", requestSchema);
+
+export default FriendRequest;
