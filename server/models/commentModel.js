3d854bbeb6cf3595f30665e9340d04123a2e5c5e
I-AnjaliKushwaha <anjalikushwaha658@gmail.com>
2024-04-21 12:09:14 +0530

    userModelSchema

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,20 +1,29 @@
 import mongoose, { Schema } from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-            userId: { type: Schema.Types.ObjectId, ref: "Users"},
-            postId: { type: Schema.Types.ObjectId, ref: "Posts"},
-            comment: { type: String, required: true },
-            from: { type: String, required: true},
-            replies: [
-                        {
-                                    rid: { type: mongoose.Schema.Types.ObjectId},
-                                    userId: { type: Schema.Types.ObjectId, ref: "Users"},
-                                    from: { type: String },
-                                    replyAt: { type: String },
-                                    comment: { tyrp: String },
-                                    created_At: { type: Date, default: Date.now()},
-                                    updated_At: { type: Date, default: Date.now()},
-                                    likes: [{ type: String }],
-                        },
-            ],
-});
+  userId: { type: Schema.Types.ObjectId, ref: "Users" },
+  postId: { type: Schema.Types.ObjectId, ref: "Posts" },
+  comment: { type: String, required: true },
+  from: { type: String, required: true },
+  replies: [
+    {
+      rid: { type: mongoose.Schema.Types.ObjectId },
+      userId: { type: Schema.Types.ObjectId, ref: "Users" },
+      from: { type: String },
+      replyAt: { type: String },
+      comment: { tyrp: String },
+      created_At: { type: Date, default: Date.now() },
+      updated_At: { type: Date, default: Date.now() },
+      likes: [{ type: String }],
+    },
+  ],
+  likes: [{ type: String }],
+},
+
+{ timestamps: true }
+
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
